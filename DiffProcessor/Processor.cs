ebc87a395f2849913fadadcc4c38471dd9593bf1
Kremen Sergey <KremenSY@gmail.com>
2015-07-02 21:55:18 +0300

    Simple-Test
    Test task C#

---
+++ DiffProcessor/Processor.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiffProcessor
+{
+	public class Processor
+	{
+		public Processor(long limit)
+		{
+			// TODO: initialize.
+		    this.limit = limit;
+		}
+
+	    private long limit;
+		public void DoProcess(SortedLimitedList<Double> mustBeEqualTo, SortedLimitedList<Double> expectedOutput)
+		{
+		    if ((limit != mustBeEqualTo.Limit) || (limit != expectedOutput.Limit))
+		    {
+		        throw new ArgumentException("Processor and both collections must have same limit");
+		    }
+			// TODO: make "mustBeEqualTo" list equal to "expectedOutput".
+			// 0. Processor will be created once and then will be used billion times. 
+			// 1. Use methods: AddFirst, AddLast, AddBefore, AddAfter, Remove to modify list.
+			// 2. Do not change expectedOutput list.
+			// 3. At any time number of elements in list could not exceed the "Limit". 
+			// 4. "Limit" will be passed into Processor's constructor. All "mustBeEqualTo" and "expectedOutput" lists will have the same "Limit" value.
+			// 5. At any time list elements must be in non-descending order.
+			// 6. Implementation must perform minimal possible number of actions (AddFirst, AddLast, AddBefore, AddAfter, Remove).
+			// 7. Implementation must be fast and do not allocate excess memory.
+
+            SortedLimitedList<Double>.Entry current1 = mustBeEqualTo.First;
+            SortedLimitedList<Double>.Entry current2 = expectedOutput.First;
+            SortedLimitedList<Double>.Entry temp;
+		    if ((mustBeEqualTo.Limit == 1) && (expectedOutput.First != mustBeEqualTo.First))
+		    {
+		        mustBeEqualTo.Remove(mustBeEqualTo.First);
+                mustBeEqualTo.AddFirst(expectedOutput.First.Value);
+		    }
+            while ((current1 != null)&&(current2!=null))
+            {  
+                if (current1.Value.CompareTo(current2.Value) == 0)
+                {
+                    current1 = current1.Next;
+                    current2 = current2.Next;
+                    continue;
+                }
+                if (current1.Value.CompareTo(current2.Value) < 0)
+                {
+                    temp = current1;
+                    current1 = current1.Next;
+                    mustBeEqualTo.Remove(temp);
+                    continue;
+                }
+                if (current1.Value.CompareTo(current2.Value) > 0)
+                {
+                    if (mustBeEqualTo.Count == mustBeEqualTo.Limit)
+                    {
+                        if (current1 == mustBeEqualTo.Last)
+                        {
+                            current1 = current1.Previous;
+                            mustBeEqualTo.Remove(mustBeEqualTo.Last);
+                            mustBeEqualTo.AddAfter(current1, current2.Value);
+                            current1 = current1.Next.Next;
+                            current2 = current2.Next;
+                            continue;
+                        }
+                        else
+                        {
+                            mustBeEqualTo.Remove(mustBeEqualTo.Last);
+                            mustBeEqualTo.AddBefore(current1, current2.Value);
+                            current2 = current2.Next;
+                        }
+                    }
+                    else
+                    {
+                        mustBeEqualTo.AddBefore(current1, current2.Value);
+                        current2 = current2.Next;
+                    }
+                }
+            }
+		    if ((current1 == null) && (current2 != null))
+		    {
+		        while (current2!=null)
+		        {
+                    mustBeEqualTo.AddLast(current2.Value);
+		            current2 = current2.Next;
+		        }
+		    }
+            if ((current1 != null) && (current2 == null))
+            {
+                while (current1 != null)
+                {
+                    temp = current1;
+                    current1 = current1.Next;
+                    mustBeEqualTo.Remove(temp);
+                }
+            }
+        }
+	}
+}
---
+++ SimpleTest/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiffProcessor;
+
+namespace SimpleTest
+{
+	class Program
+	{
+        //static Processor diffProcessor = new Processor(1);
+        //static SortedLimitedList<Double> sortedList1 = new SortedLimitedList<Double>(1);
+        //static SortedLimitedList<Double> sortedList2 = new SortedLimitedList<Double>(1);
+        //static SortedLimitedList<Double> sortedList1_ = new SortedLimitedList<Double>(1);
+        //static SortedLimitedList<Double> sortedList2_ = new SortedLimitedList<Double>(1);
+
+        //static Processor diffProcessor = new Processor(8);
+        //static SortedLimitedList<Double> sortedList1 = new SortedLimitedList<Double>(10);
+        //static SortedLimitedList<Double> sortedList2 = new SortedLimitedList<Double>(10);
+        //static SortedLimitedList<Double> sortedList1_ = new SortedLimitedList<Double>(10);
+        //static SortedLimitedList<Double> sortedList2_ = new SortedLimitedList<Double>(10);
+
+        static Processor diffProcessor = new Processor(10);
+        static SortedLimitedList<Double> sortedList1 = new SortedLimitedList<Double>(10);
+        static SortedLimitedList<Double> sortedList2 = new SortedLimitedList<Double>(10);
+        static SortedLimitedList<Double> sortedList1_ = new SortedLimitedList<Double>(10);
+        static SortedLimitedList<Double> sortedList2_ = new SortedLimitedList<Double>(10); 
+        static Int64 test = 0;
+		static void DoTest(Int32 operations)
+		{
+			try
+			{
+				diffProcessor.DoProcess(sortedList1, sortedList2);
+				if (!sortedList1.Equals(sortedList2) || !sortedList1.Equals(sortedList2_) || sortedList1.PerformedOperations != operations)
+				{
+					Console.WriteLine("Test case: " + ++test + " Failed. Inputs: mustBeEqual: [" + sortedList1_ + "] expected: [" + sortedList2_ + "]");
+					Console.WriteLine("Your output " + sortedList1);
+					Console.WriteLine("Performed operations: " + sortedList1.PerformedOperations + ", expected " + operations);
+					Console.WriteLine();
+				}
+				else
+				{
+					Console.WriteLine("Test case: " + ++test + " Succeed. Inputs: mustBeEqual: [" + sortedList1_ + "] expected: [" + sortedList2_ + "]");
+					Console.WriteLine();
+				}
+			} 
+			catch (Exception)
+			{
+				Console.WriteLine("Test case: " + ++test + " Exception. Inputs: mustBeEqual: [" + sortedList1_ + "] expected: [" + sortedList2_ + "]");
+				Console.WriteLine();
+			}
+		}
+
+		static void Test(Double[] array1, Double[] array2, Int32 operations)
+		{
+			sortedList1.FromArray(array1);
+			sortedList2.FromArray(array2);
+			sortedList1_.FromArray(array1);
+			sortedList2_.FromArray(array2);
+			DoTest(operations);
+		}
+
+		static void Test(List<Double> list1, List<Double> list2, Int32 operations)
+		{
+			sortedList1.FromList(list1);
+			sortedList2.FromList(list2);
+			sortedList1_.FromList(list1);
+			sortedList2_.FromList(list2);
+			DoTest(operations);
+		}
+
+
+		static void Main(String[] args)
+		{
+            Double[] needToBeEqual_0 = { 0, 1, 2, 3, 4, 6, 7 };
+            Double[] expected_0 = { 0, 1, 2, 3, 4, 5, 6, 7 };
+            Test(needToBeEqual_0, expected_0, 1);
+
+            Double[] needToBeEqual_1 = { 0, 1, 2, 3, 4, 6, 7 };
+            Double[] expected_1 = { 1, 2, 3, 3 };
+            Test(needToBeEqual_1, expected_1, 5);
+
+            Double[] needToBeEqual_2 = { 0 };
+            Double[] expected_2 = { 7 };
+            Test(needToBeEqual_2, expected_2, 2);
+
+            Double[] needToBeEqual_3 = { 0, 1, 1, 1, 1, 1, 1 };
+            Double[] expected_3 = { 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 };
+            Test(needToBeEqual_3, expected_3, 17);
+
+            Double[] needToBeEqual_4 = { 0, 1, 1, 1, 1, 1, 1, 1, 1, 3 };
+            Double[] expected_4 = { 0, 2, 2, 2, 2, 2, 2, 2, 2, 3 };
+            Test(needToBeEqual_4, expected_4, 16);
+
+            Double[] needToBeEqual_5 = { 0, 1, 2, 3, 4, 6, 7 };
+            Double[] expected_5 = { };
+            Test(needToBeEqual_5, expected_5, 7);
+
+            Double[] needToBeEqual_6 = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
+            Double[] expected_6 = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            Test(needToBeEqual_6, expected_6, 2);
+
+            Double[] needToBeEqual_7 = { 0, 0 };
+            Double[] expected_7 = { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            Test(needToBeEqual_7, expected_7, 10);
+
+            Double[] needToBeEqual_8 = { 1 };
+            Double[] expected_8 = { 0 };
+            Test(needToBeEqual_8, expected_8, 2);
+        }
+	}
+}
